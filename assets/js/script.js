385b8ad617afa326404d1111f71444b554d42aa5
Nico Croce <ingcrocenicolas@outlook.com>
2016-09-23 18:42:16 -0300

    WIP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
 
 $(document).ready(function () {
     $('#changeSection').load('./partials/home.html');
-})
+
+    $('#btMenu').click(function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            $('html').removeClass('no-scroll');
+            $('#menuMobile').addClass('hidden');
+            $('#bgPage').removeClass('visible');
+        }else{
+            $(this).addClass("active");
+            $('html').addClass('no-scroll');
+            $('#menuMobile').removeClass('hidden');
+            $('#bgPage').addClass('visible');
+        }   
+    });
+});
